b4fb75b14fa009a853f7f6dc8f4c4a6c1c486aa5
selcukarz <ariozselcuk@gmail.com>
2022-10-19 16:16:09 +0300

    first commit

--- Gesture Recognizer/ViewController.swift
+++ Gesture Recognizer/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var myLabel: UILabel!
+
+    var metallica1 = true
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageView.isUserInteractionEnabled = true
+        let gastureRegonnizer = UITapGestureRecognizer(target: self, action: #selector(changePic))
+        imageView.addGestureRecognizer(gastureRegonnizer)
     }
 
-
+    @objc func changePic() {
+        if metallica1 == true{
+            imageView.image = UIImage(named: "metallica1")
+            myLabel.text = "first pic"
+            metallica1 = false
+        }else {
+            imageView.image = UIImage(named: "Metallica2")
+            myLabel.text = "second pic"
+            metallica1 = true
+        }
+    }
 }
 
